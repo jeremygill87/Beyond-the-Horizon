438398bf837e95ead100c14b8973b568a0384bb4
Jeremy Gill <54640283+jeremygill87@users.noreply.github.com>
2022-01-13 11:10:40 -0500

    Created Basic Columns for Background
    Enemy Status and Enemy Avatar.  Working on sizing and positioning next, plus ensuring Chrome launch environment is working.

--- src/components/BattleBoxes/Background.js
+++ src/components/BattleBoxes/Background.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
 class Background extends Component {
-    
+    render() {
+    <Container fluid>
+        <Row>
+            <Col>Enemy Status</Col>
+            <Col>Enemy Avatar(s)</Col>
+        </Row>
+    </Container>
+    }
 }
