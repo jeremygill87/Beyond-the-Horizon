2e11665fd82b9b3ae91ecb8ef19d163146ba85ed
Jeremy Gill <54640283+jeremygill87@users.noreply.github.com>
2022-01-13 13:16:18 -0500

    Setting Up Columns and Rows in Background
    Imported react-bootstrap and necessary components.  Began putting together width and height to accommodate various input boxes

--- src/components/BattleBoxes/Background.js
+++ src/components/BattleBoxes/Background.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Col, Row, Container } from 'react-bootstrap'
 
-class Background extends Component {
+class Background extends React.Component {
     render() {
-    <Container fluid>
+        return (
+            <Container fluid>
         <Row>
             <Col>Enemy Status</Col>
             <Col>Enemy Avatar(s)</Col>
         </Row>
+        <Row>
+            <Col>Player Avatars</Col>
+            <Col>Player Status</Col>
+        </Row>
+        <Row>
+            <Col>PLAYER SELECTION</Col>
+        </Row>
     </Container>
+        )
+    
     }
 }
+
+export default Background;
