a4d353df2106845e0871570167e954fe00a66af0
Jeremy Gill <54640283+jeremygill87@users.noreply.github.com>
2022-02-02 14:51:11 -0500

    set up component for TopDown Map generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Background from './components/BattleBoxes/Background'
+import TDMap from './components/TopDownVIew/TDMove';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Background/>
+        <TDMap></TDMap>
       </div>
     )
   }
